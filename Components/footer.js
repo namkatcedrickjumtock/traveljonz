6ae537ea06f0c926f224e2dfd36b32fcd07522d7
Namkat Cedrick j <namkatcedrickjumtock3@gmail.com>
2022-06-25 16:59:33 +0100

    search screen implemented and useEffect validation

--- Components/footer.js
+++ Components/footer.js
@@ -4,8 +4,8 @@ import Script from "next/script";
 export default function Footer() {
     return (
         <div>
-            <footer className='bg-dark'>
-                <p className="text-center text-white">@Copy rights 2022 | TravelJonz LLC</p>
+            <footer className='card-footer'>
+                <p className="text-center text-">@Copy rights 2022 | TravelJonz LLC</p>
             </footer>
             <Script src={'https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js'}></Script>
             <Script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" />
--- pages/Searched.js
+++ pages/Searched.js
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
 
 const Searched = () => {
+
+    const [CountryFields, setCountryFields] = useState('');
+    const [state, setStateFields] = useState('');
+
+    useEffect((e) => {
+        if (CountryFields >= 3) {
+            console.log("use Effect worked on changed");
+        } else {
+            return
+        }
+        return () => {
+            cleanup
+        };
+    }, [CountryFields]);
+
+    const HandleSubmit = (e) => {
+        e.preventDefault();
+        const formData = { CountryFields, state }
+        console.log(formData)
+
+
+
+    }
+
     return (
-        <div>
-            <h4>Searched</h4>
+        <div className='container'>
+            <img src="/img/search-img.svg" alt="" className="mx-auto d-block  py-4" height={'300vh'} width={'100%'} />
+            <form className='py-3' onSubmit={HandleSubmit}>
+                <div className="row">
+                    <div className="col-md-8 mb-3">
+                        <input
+                            value={CountryFields}
+                            type="search"
+                            onChange={(e) => setCountryFields(e.target.value)}
+                            className="form-control shadow-none"
+                            placeholder="Country" aria-label="Search" />
+                        <small className="text-muted">{CountryFields}</small>
+                    </div>
+                    <div className="col-md-4">
+                        <input type="search"
+                            value={state}
+                            className="form-control shadow-none"
+                            onChange={(e) => setStateFields(e.target.value)}
+                            placeholder="State" aria-label="Search" />
+                        <small className="text-muted">{state}</small>
+
+                    </div>
+                </div>
+                <button className=" btn btn-outline-primary mx-auto d-block my-3 w-25 disable" type='submit'>search</button>
+            </form>
         </div>
     );
 }
